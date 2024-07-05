d2e21f6639799a8d91305dc04bc05c0f7dc93548
liujip0 <liujip2020@gmail.com>
2024-07-05 18:28:02 +0800

    Started work on sort function for lexicon

--- morpheme/src/common/Vals.tsx
+++ morpheme/src/common/Vals.tsx
@@ -39,7 +39,8 @@ export const conlangInit: Conlang = {
       }
     ],
     foldersOnTop: false
-  }
+  },
+  lexicon: []
 };
 
 interface StoreState {
--- morpheme/src/screens/LexiconScreen.tsx
+++ morpheme/src/screens/LexiconScreen.tsx
@@ -1,8 +1,71 @@
 import { useState } from 'react';
+import { MdAdd } from 'react-icons/md';
+import { IconButton } from '../common/Components.tsx';
 import { useStoreState } from '../common/Vals.tsx';
 
 export default function LexiconScreen() {
+  const conlang = useStoreState((s) => s.conlang);
+  const changeConlang = useStoreState((s) => s.changeConlang);
   const [currentWord, setCurrentWord] = useState('');
+  const createAlphabetMap = (
+    alphabetArray: Array<string>
+  ): Map<string, number> => {
+    const alphabetMap = new Map();
+    for (let i = 0; i < alphabetArray.length; i++) {
+      alphabetMap.set(alphabetArray[i], i);
+    }
+    return alphabetMap;
+  };
+  const alphabeticalOrder = createAlphabetMap(
+    conlang.phonology.inventory.map((x) => x.romanization)
+  );
+  const customComparator = (alphabetMap: Map<string, number>) => {
+    return (a: string, b: string) => {
+      let indexA, indexB;
+      let i = 0,
+        j = 0;
+      while (i < a.length && j < b.length) {
+        let foundA = false,
+          foundB = false;
+        for (const [key, value] of alphabetMap.entries()) {
+          if (a.startsWith(key, i)) {
+            indexA = value;
+            foundA = true;
+          }
+          if (b.startsWith(key, j)) {
+            indexB = value;
+            foundB = true;
+          }
+          if (foundA && foundB) break;
+        }
+        if (!foundA) indexA = -1;
+        if (!foundB) indexB = -1;
+        if (indexA !== indexB) {
+          return indexA! - indexB!;
+        }
+        i += Math.max(a.startsWith(key, i) ? key.length : 1, 1);
+        j += Math.max(b.startsWith(key, j) ? key.length : 1, 1);
+      }
+      return a.length - b.length; // If all parts are the same, compare by length
+    };
+  };
+  const sortLexicon = () => {
+    const newLexicon = conlang.lexicon;
+    newLexicon.sort((a, b) => {});
+    changeConlang(['lexicon'], newLexicon);
+  };
+  const changeWord = (id: string, property: string, newValue: unknown) => {
+    const index = conlang.articles.list.findIndex((value) => value.id === id);
+    const newArticles = conlang.articles.list;
+    if (index !== -1) {
+      newArticles.splice(index, 1, {
+        ...conlang.articles.list[index],
+        [property]: newValue
+      });
+      changeConlang(['articles', 'list'], newArticles);
+      sortLexicon();
+    }
+  };
   return (
     <div
       style={{
@@ -23,9 +86,26 @@ type WordsProps = {
 };
 function Words({ currentWord, setCurrentWord }: WordsProps) {
   const conlang = useStoreState((s) => s.conlang);
-  const alphabeticalOrder = conlang.phonology.inventory.map(
-    (x) => x.romanization
-  );
+  const addArticle = (article: Folder | Article) => {
+    const datetime = new Date();
+    const id =
+      article.type +
+      datetime.getHours() +
+      '-' +
+      datetime.getMinutes() +
+      '-' +
+      datetime.getSeconds() +
+      '-' +
+      datetime.getMilliseconds();
+    const newArticles = conlang.articles.list;
+    newArticles.push({
+      ...article,
+      id: id
+    });
+    changeConlang(['articles', 'list'], newArticles);
+    sortArticles();
+    return id;
+  };
   return (
     <div
       style={{
@@ -41,7 +121,11 @@ function Words({ currentWord, setCurrentWord }: WordsProps) {
           marginBottom: '1em',
           display: 'flex',
           justifyContent: 'space-around'
-        }}></div>
+        }}>
+        <IconButton onClick={() => {}}>
+          <MdAdd />
+        </IconButton>
+      </div>
       <div
         style={{
           backgroundColor: 'white',
