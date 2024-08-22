d9801e45970f11c224729271f9eb300ae498ca2c
liujip0 <liujip2020@gmail.com>
2024-08-22 00:37:50 -0400

    some work on pwa

--- morpheme/vite.config.ts
+++ morpheme/vite.config.ts
@@ -1,8 +1,36 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/morpheme/'
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false,
+      },
+      includeAssets: ['./vite.svg'],
+      manifest: {
+        name: 'Morpheme',
+        short_name: 'Morpheme',
+        description: 'An app to help conlangers create conlangs',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: './vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/morpheme/',
 });
